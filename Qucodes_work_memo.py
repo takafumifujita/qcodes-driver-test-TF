06c45ac78c827d4cfa1409b4b11fa3be9b30ce11
takafumifujita <t.fujita@tudelft.nl>
2016-11-16 19:53:31 +0100

    first commit to create files

---
+++ Qucodes_work_memo.py
@@ -0,0 +1,20 @@
+
+
+
+
+What I want to do now.
+
+	make drivers for the fridge
+	push so that we can put it in qtt? or qucodes?
+		ask sjaak for this
+	is there a way to put it on git so that I can work on it personally?
+		upload my folder in git
+
+
+Install Anaconda  # easy to get most of the dependencies
+
+
+
+
+
+
